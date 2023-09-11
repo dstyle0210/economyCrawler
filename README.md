a347f23b9cdf952ed9ce2acc45de7c8522046ecd
dstyle0210 <dstyle0210@gmail.com>
2023-09-11 11:42:47 +0900

    매일경제

---
+++ README.md
@@ -0,0 +1,13 @@
+# economyCrawler
+github의 action을 이용해서, 경제지표, 경제뉴스 텔레그램으로 받기.
+- 오전 9시
+- 매일경제 : 매-세-지
+  
+```powershell
+> Set-ExecutionPolicy RemoteSigned
+```
+
+```console
+> git config --global user.name dstyle0210
+> git config --global user.email dstyle0210@gmail.com
+```
---
+++ gulpfile.ts
@@ -0,0 +1,62 @@
+import {task} from "gulp";
+import {chromium,devices} from "playwright";
+const TelegramBot = require('node-telegram-bot-api');
+const token = '6168835435:AAEX-jYqum2mD4N2ath6_QihrqjPC5GJ-C4';
+const chatId = 6252259316;
+
+task("test",async (done) => {
+    const browser = await chromium.launch({headless:true});
+    const page = await browser.newPage();
+    await page.goto("https://www.mk.co.kr/search?word=%EB%A7%A4-%EC%84%B8-%EC%A7%80");
+    const todayNews = await page.evaluate(() => {
+        const news = $(".news_item").filter((idx,item) => {
+            return (/매경이 전하는 세상의 지식/).test( $(item).find(".news_ttl").text() );
+        });
+        return news.eq(0).attr("href");
+    });
+    console.log(todayNews);
+
+    // 텔레그램봇 시작
+    const bot = new TelegramBot(token, {polling: false});
+    bot.sendMessage(chatId, todayNews);
+
+
+    // await new Promise((resolve)=>setTimeout(resolve,660000)); // 11분 후 닫음 (텔레그램은 연결 후 10분 이내 끊을경우 429에러 발생함 , Error: ETELEGRAM: 429 Too Many Requests: retry after 599)
+    await new Promise((resolve)=>setTimeout(resolve,1000)); // 개발용 1초 있다가 닫음 (그냥 에러 나는걸로..)
+    
+    await browser.close();
+    // bot.sendMessage(chatId, "닫습니다");
+    // await bot.close();
+    done(); 
+});
+
+
+// firebase
+/*
+import { initializeApp } from 'firebase/app';
+import { getDatabase , set , ref ,onValue } from 'firebase/database';
+task("test2",(done) => {
+    const firebaseConfig = {
+        databaseURL: "https://dstyle-action-crawler-default-rtdb.firebaseio.com",
+    };
+    const app = initializeApp(firebaseConfig);
+    const db = getDatabase(app);
+    const dbRef = ref(db, 'users/');
+
+    // read
+    onValue(dbRef,(snapshot) => {
+        const data = snapshot.val();
+        console.log(data);
+        done();
+        process.exit(0);
+    })
+    // create , update 
+    set(dbRef, {
+        username: "asd",
+        email: "이메일44",
+        profile_picture : "src"
+      }).then(()=>{
+        done();
+      });
+})
+*/
