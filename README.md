5307201e230e1c7c6d801bc4cf5bc362c9899f12
dstyle0210 <dstyle0210@gmail.com>
2023-09-11 13:29:51 +0900

    한경추가,시간변경

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # economyCrawler
 github의 action을 이용해서, 경제지표, 경제뉴스 텔레그램으로 받기.
-- 오전 9시
+- 오전 7시 30분 (cron 30 22 * * 0-4) / github가 UTC+0 을 기준함.
 - 매일경제 : 매-세-지
+- 한국경제 : 모닝브리핑
   
 ```powershell
 > Set-ExecutionPolicy RemoteSigned
--- gulpfile.ts
+++ gulpfile.ts
@@ -7,23 +7,31 @@ const chatId = 6252259316;
 task("test",async (done) => {
     const browser = await chromium.launch({headless:true});
     const page = await browser.newPage();
+
+    await page.goto("https://www.hankyung.com/tag/%EB%AA%A8%EB%8B%9D%EB%B8%8C%EB%A6%AC%ED%95%91");
+    const hankyungNews = await page.evaluate(() => {
+        return $(".news-tit a").eq(0).attr("href");
+    });
+
     await page.goto("https://www.mk.co.kr/search?word=%EB%A7%A4-%EC%84%B8-%EC%A7%80");
-    const todayNews = await page.evaluate(() => {
+    const mkNews = await page.evaluate(() => {
         const news = $(".news_item").filter((idx,item) => {
             return (/매경이 전하는 세상의 지식/).test( $(item).find(".news_ttl").text() );
         });
         return news.eq(0).attr("href");
     });
-    console.log(todayNews);
-
     // 텔레그램봇 시작
     const bot = new TelegramBot(token, {polling: false});
-    bot.sendMessage(chatId, todayNews);
 
+     // 텔레그램 발송
+     const cardList = [hankyungNews,mkNews];
+     for(let card of cardList){
+        bot.sendMessage(chatId, "[NEWS] "+card);
+    };
 
     // await new Promise((resolve)=>setTimeout(resolve,660000)); // 11분 후 닫음 (텔레그램은 연결 후 10분 이내 끊을경우 429에러 발생함 , Error: ETELEGRAM: 429 Too Many Requests: retry after 599)
     await new Promise((resolve)=>setTimeout(resolve,1000)); // 개발용 1초 있다가 닫음 (그냥 에러 나는걸로..)
-    
+
     await browser.close();
     // bot.sendMessage(chatId, "닫습니다");
     // await bot.close();
